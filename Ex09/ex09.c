bea66b48c20ff0d2fa782abe652018fd6a0ec166
Rafaeldias0934 <74202654+Rafaeldias0934@users.noreply.github.com>
2021-06-01 19:59:33 -0300

    velocidade média em relação a via.

---
+++ Ex09/ex09.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() 
+{
+    int velocidade_media;
+    printf("Digite a sua velocidade na viagem em (km/h):\n");
+    scanf("%d",&velocidade_media);
+
+if (velocidade_media > 110)
+    printf("A velocidade média é %d, então ela é superior a 110km/h.\n",velocidade_media);
+else
+     printf("A velocidade média é %d, então ela é não superior a 110km/h.\n",velocidade_media);
+ 
+ return 0; 
+}
