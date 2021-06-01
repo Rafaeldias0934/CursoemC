e93f60d08f8dd54eb2ac32a65f3d8a66713d3802
Rafaeldias0934 <74202654+Rafaeldias0934@users.noreply.github.com>
2021-06-01 18:49:22 -0300

    Exercícios Linguagem C

---
+++ Ex08/ex08.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{ 
+  float med1,med2,med3,ma, mp;
+  int op;
+
+
+printf("Digite a sua média da primeira unidade:\n");
+scanf("%f",&med1);
+
+printf("Difgite a sua média da segunda unidade:\n");
+scanf("%f", &med2);
+
+printf("Digite a sua média da terceira unidade:\n");
+scanf("%f",&med3);
+
+ma = (med1 + med2 + med3) / 3;
+mp = (med1 * 3 + med2 * 3 + med3 * 4) / 10;
+
+printf("Digite 1 para média Aritmetica, ou 2 para média Ponderada:\n");
+scanf("%d",&op);
+
+switch (op)
+{
+case 1:
+  printf("Sua média Aritmetica é %2.1f:\n ",ma);
+  break;
+
+case 2:
+  printf("Sua média Ponderada é %2.1f:\n ",mp);
+  break;
+
+default:
+  printf("%d Número Invalido ",op);
+  break;
+}
+return 0;
+}
