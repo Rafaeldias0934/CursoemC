147ff56fd6b112289d5a91e69368a38a952a42db
Rafaeldias0934 <74202654+Rafaeldias0934@users.noreply.github.com>
2021-06-01 19:03:15 -0300

    Modificando o desafio

--- Ex02/desafio.c
+++ Ex02/desafio.c
@@ -18,27 +18,6 @@ int main () {
     return 0;
 }
 
-/*
-#include<stdio.h>
-int main()
-{ 
-  int x,y;
-
-
-printf("\n Digite um número para x:\n");
-scanf("%d",&x); 
-
-printf("\n Digite um número para y:\n");
-scanf("%d",&y); 
-
-if (x % y == 0 && x % 3 ==0)
-    printf("O número digitado é %d, então ele é Par e divisivel por 3.",x);
-else
-     printf("O número digitado é %d, logo não é Par, e nem Divisivel por 3.",x);
- 
- return 0; 
-}
-*/
 
 /*
 
