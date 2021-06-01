2bfd8509ba92e4ff39248efe7f617f90ec4ad75d
Rafaeldias0934 <74202654+Rafaeldias0934@users.noreply.github.com>
2021-06-01 19:40:27 -0300

    Ano em que o úsuario que saber sua idade.

--- Ex05/ex05.c
+++ Ex05/ex05.c
@@ -5,18 +5,18 @@
     char nome[8];
     int ano_nascimento,ano_atual, ano_selecionado,idade;
     
-    printf("Digite seu nome:\n ");
+    printf("Digite seu nome:\n");
     scanf("%s",nome);
 
     printf("Bem vindo %s!\n",nome);
 
-    printf("Digite agora sua idade:\n ");
+    printf("Digite agora sua idade:\n");
     scanf("%i",&idade);
     
-    printf("Digite o ano que voce nasceu: ");
+    printf("Digite o ano que voce nasceu:\n");
     scanf("%i",&ano_nascimento);
 
-    printf("Digite o ano que voce quer saber sua idade: ");
+    printf("Digite o ano que voce quer saber sua idade:\n");
     scanf("%i",&ano_selecionado);
 
     ano_atual = (ano_selecionado - ano_nascimento);
