ca57037fa74560ee9b31dbf3fe07fd7b49b3a912
Rafaeldias0934 <74202654+Rafaeldias0934@users.noreply.github.com>
2021-06-01 20:49:38 -0300

    Número Par / Impar e Divisível por 3 ou não.

--- Ex12/ex12.c
+++ Ex12/ex12.c
@@ -9,19 +9,19 @@ scanf("%d",&x);
 
 if (x % 2 == 0 && x % 3 ==0)
 {
-    printf("O número digitado é %d, então ele é Par e divisível por 3.",x);
+    printf("O número digitado é %d, logo é Par, e divisível por 3.\n",x);
 }
 else if (x % 2 == 0 && x % 3 ==1)
 {
-    printf("O número digitado é %d, logo é Par, e não é divisível por 3.",x);
+    printf("O número digitado é %d, logo é Par, e não é divisível por 3.\n",x);
 }
 else if (x % 2 == 1 && x % 3 ==0)
 {
-    printf("O número digitado é %d, logo é Impar, e Divisível por 3.",x);
+    printf("O número digitado é %d, logo é Impar, e divisível por 3.\n",x);
 }
-else if (x % 2 == 1 && x % 3 ==1)
+else 
 {
-    printf("O número digitado é %d, logo é Impar, e não é divisível por 3.",x);
+    printf("O número digitado é %d, logo é Impar, e não é divisível por 3.\n",x);
 }
  return 0; 
 }
