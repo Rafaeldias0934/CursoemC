4f860240cde55a356249622af5c1e6c662497136
Rafaeldias0934 <74202654+Rafaeldias0934@users.noreply.github.com>
2021-07-06 08:56:21 -0300

    Atualizad0 - (Em andamento).

--- Ex01/file.c
+++ Ex01/file.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define TENTATIVAS 5
 
 int main () {
     //imprime o cabeçalho do nosso jogo
@@ -8,8 +9,9 @@ int main () {
 
     int chute,numerosecreto = 42, i;
    
-    for (i = 1; i <= 3; i++)    
+    for (i = 1; i <= TENTATIVAS; i++)    
     {
+        printf("tentativa %d de %d\n", i,TENTATIVAS);
         printf("qual é o chute?\n");
         scanf("%d", &chute);
         printf("Seu chute foi %d\n", chute);
@@ -19,6 +21,7 @@ int main () {
         if (acertou) {
             printf("Parabens, Você acertou!!\n");
             printf("Jogue de novo, voce é um bom jogador!!\n");
+            break;
         }
         else {
             
@@ -32,41 +35,5 @@ int main () {
             }
         }     
     }
-  
+    printf("Fim do jogo\n");
 }
-
-/*
-#include <stdio.h>
-
-int main () {
-    //imprime o cabeçalho do nosso jogo
-    printf("*********************************\n");
-    printf("*Bem vindo ao jogo de Advinhação*\n");
-    printf("*********************************\n");
-
-    int chute,numerosecreto = 42, i = 1;
-   
-   while(i<=3) {
-        printf("Qual é o seu chute? ");
-        scanf("%d", &chute);
-        printf("Seu chute foi %d\n", chute);
-    
-        int acertou = (chute == numerosecreto);
-    
-        if (acertou) {
-            printf("Parabéns! Você acertou!\n");
-            printf("Jogue de novo, você é um bom jogador!\n");
-        }
-        else {
-            int maior = chute > numerosecreto;
-            if(maior) {
-                printf("Seu chute foi maior que o número secreto\n");
-            } else {
-                printf("Seu chute foi menor que o número secreto\n");
-            }
-        }
-        i++;
-   }
-    printf("Fim do jogo!!");
-}
-*/
